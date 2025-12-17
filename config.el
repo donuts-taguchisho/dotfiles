db07293fdf38a3ab3ea3f40f2da5f5082fcb6fb9
shoh13 <10_draft_willful@icloud.com>
2025-12-18 02:05:56 +0900

    set lsp-treemacs key map

--- config.el
+++ config.el
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq! :leader
+       :desc "Toggle lsp-treemacs"
+       "o p" #'treemacs)
