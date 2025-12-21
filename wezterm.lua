97097f7ebc80919cc2b288166cab0f554a894c0b
shoh13 <10_draft_willful@icloud.com>
2025-12-21 23:47:15 +0900

    add fancy_tab_bar and tab_bar_background opacity

--- wezterm.lua
+++ wezterm.lua
@@ -9,5 +9,11 @@ config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.colors = {
+  tab_bar = {
+    background = "none",
+  },
+}
 
 return config
