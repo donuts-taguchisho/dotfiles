92c339b8ddba6e816dc276153728a5f3e91108fe
shoh13 <10_draft_willful@icloud.com>
2025-12-21 23:40:24 +0900

    add background blur

--- wezterm.lua
+++ wezterm.lua
@@ -6,5 +6,6 @@ config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime  = true
 config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
 
 return config
