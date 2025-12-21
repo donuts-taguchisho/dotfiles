25ada2de25e8849f966efa3a4a11e1cea5c9c58a
shoh13 <10_draft_willful@icloud.com>
2025-12-21 23:36:53 +0900

    adjust font size

--- wezterm.lua
+++ wezterm.lua
@@ -1,18 +1,8 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+config.automatically_reload_config = true
+config.font_size = 12.0
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
-
--- or, changing the font size and color scheme.
-config.font_size = 10
-config.color_scheme = 'AdventureTime'
-
--- Finally, return the configuration to wezterm:
 return config
