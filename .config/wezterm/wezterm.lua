f3ae38e9f6f1ad87880a6fbd356d553a2fdfc964
shoh13 <10_draft_willful@icloud.com>
2026-02-05 23:09:52 +0900

    delete start_as config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.start_as = 'Maximized'
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime  = true
