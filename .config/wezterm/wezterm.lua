c08bcd30d7c6c0fe392e98bb74330669548b47ed
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2026-02-13 11:02:48 +0900

    change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,23 +1,27 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.font_size = 12.0
-config.use_ime  = true
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Nerd Font Symbols",
+})
+config.use_ime = true
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.colors = {
-  tab_bar = {
-    background = "none",
-  },
+	tab_bar = {
+		background = "none",
+	},
 }
 config.window_background_gradient = {
-   colors = { "#000000" },
- }
+	colors = { "#000000" },
+}
 config.show_new_tab_button_in_tab_bar = false
 
 config.keys = require("keybinds").keys
@@ -26,8 +30,8 @@ config.disable_default_key_bindings = true
 config.leader = { key = " ", mods = "CTRL", timeout_milliseconds = 10000 }
 
 config.set_environment_variables = {
-  TERM = 'xterm-256color',
-  COLORTERM = 'truecolor',
+	TERM = "xterm-256color",
+	COLORTERM = "truecolor",
 }
 
 return config
